4c5374447aba2a2d06436cffc122bea7b50fdda0
srartese <saa2457@rit.edu>
2017-11-04 15:21:49 -0400

    Get that friend list up

--- frontEnd/server/components/friends.js
+++ frontEnd/server/components/friends.js
@@ -1,18 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom";
 
 
-// Create a component named MessageComponent
-var MessageComponent = React.createClass({
-  render: function() {
-    return (
-      <p>aasdasd</p>
-    );
-  }
-});
-
-// Render an instance of MessageComponent into document.body
-ReactDOM.render(
-  <MessageComponent message="Hello!" />,
-  document.body
-);
---
+++ newFrontEnd/script.js
@@ -0,0 +1,9 @@
+"use strict";
+$(document).ready(function(){
+    $('.jessesaran').click(function () {
+        console.log("click");
+        $('.mainScreen').hide();
+        $('.confirmShare').show();  
+    });
+
+});
