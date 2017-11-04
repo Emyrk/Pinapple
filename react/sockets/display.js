a84ebecdef329ee3af4e59f941ad831d57f20517
Steven Masley <stevenmasley@gmail.com>
2017-11-04 09:49:44 -0400

    Added Jquery

---
+++ react/sockets/display.js
@@ -0,0 +1,31 @@
+$(".draggable").draggable({ cursor: "crosshair", revert: "invalid"});
+$("#drop").droppable({ accept: ".draggable", 
+           drop: function(event, ui) {
+                    console.log("drop");
+                   $(this).removeClass("border").removeClass("over");
+             var dropped = ui.draggable;
+            var droppedOn = $(this);
+            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);      
+             
+             
+                }, 
+          over: function(event, elem) {
+                  $(this).addClass("over");
+                   console.log("over");
+          }
+                ,
+                  out: function(event, elem) {
+                    $(this).removeClass("over");
+                  }
+                     });
+$("#drop").sortable();
+
+$("#origin").droppable({ accept: ".draggable", drop: function(event, ui) {
+                    console.log("drop");
+                   $(this).removeClass("border").removeClass("over");
+             var dropped = ui.draggable;
+            var droppedOn = $(this);
+            $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);      
+             
+             
+                }});
